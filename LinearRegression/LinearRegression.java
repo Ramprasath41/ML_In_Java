2b5b6158d10c1bbdb909a2ecc5f5614c4a576460
ramprasath41 <ramprasath4170@gmail.com>
2026-06-17 00:34:57 +0530

    Staring some Calculations

---
+++ LinearRegression/LinearRegression.java
@@ -0,0 +1,27 @@
+class LinearRegression{
+    private double[] independentVariable;
+    private double[] dependentVariable;
+    private int n;
+    private double sumOfX;
+    private double sumOfY;
+    LinearRegression(double[] independentVariable, double[] dependentVariable){
+        this.n = independentVariable.length();
+        this.independentVariable = independentVariable;
+        this.dependentVariable = dependentVariable;
+        calculations();
+    }
+
+    private double calculateSlope(){
+        double slope =0;
+    }
+
+    private double calculateIntercept(){
+        double[] intercept = new intercept[n];
+    }
+
+    private double calculations(){
+        this.sumOfX = Arrays.stream.(independentVariable).sum();
+        this.sumOfY = Arrays.stream.(dependentVariable).sum();
+
+    }
+}
